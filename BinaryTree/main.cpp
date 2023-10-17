76c369f782e54b8c5e596f3f76ab7bc0ce41420d
Artem <aztec@DESKTOP-CK171NA>
2023-10-17 12:41:44 +0400

    hw in progress

--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -34,6 +35,7 @@ public:
 	}
 	~Tree()
 	{
+		deleteall();
 		cout << "TDestructor:\t" << this << endl;
 	}
 
@@ -52,27 +54,62 @@ public:
 			else insert(Data, Root->pRight);
 		}
 	}
+	void insert(int Data)
+	{
+		insert(Data, Root);
+	}
 	int minValue(Element* Root)
 	{
 		return Root->pLeft == nullptr ? Root->Data : minValue(Root->pLeft);
+	}
+	int minValue()
+	{
+		return minValue(Root);
 	}
 	int maxValue(Element* Root)
 	{
 		return Root->pRight ? maxValue(Root->pRight) : Root->Data;
 	}
+	int maxValue()
+	{
+		return maxValue(Root);
+	}
 	int Sum(Element* Root)
 	{
 		return Root ? Root->Data + Sum(Root->pLeft) + Sum(Root->pRight) : 0;
+	}
+	int Sum()
+	{
+		return Sum(Root);
 	}
 	int Count(Element* Root)
 	{
 		return Root ? 1 + Count(Root->pLeft) + Count(Root->pRight) : 0;
 	}
+	int Count()
+	{
+		return Count(Root);
+	}
 	double Avg()
 	{
 		return (double)Sum(Root) / Count(Root);
 	}
-
+	void deleteall(Element* Root)
+	{
+		if (Root == nullptr)return;
+		else
+		{
+			deleteall(Root->pLeft);
+			deleteall(Root->pRight);
+			delete Root;
+			Root = nullptr;
+		}
+	}
+	void deleteall()
+	{
+		deleteall(Root);
+	}
+	
 	void print(Element* Root)const
 	{
 		if (Root == nullptr)return;
@@ -80,7 +117,40 @@ public:
 		cout << Root->Data << tab;
 		print(Root->pRight);
 	}
+	void print()const
+	{
+		print(Root);
+	}
 };
+
+class UniqueTree :public Tree
+{
+	std::vector<int> exists;
+	static int skipped;
+public:
+	UniqueTree() :Tree() { cout << "UTConstructor:\t" << this << endl; }
+	~UniqueTree() { cout << "UTDestructor:\t" << this << endl; }
+	void insert(int Data)
+	{
+		if (std::find(exists.begin(), exists.end(), Data) != exists.end()) 
+		{
+			skipped++;
+			return;
+		}
+		else
+		{
+			Tree::insert(Data);
+			exists.push_back(Data);
+		}
+	}
+	void print()const
+	{
+		Tree::print();
+		cout << "\nВсего пропущено " << skipped << " повторяющихся элементов" << endl;
+	}
+};
+
+int UniqueTree::skipped = 0;
 
 void main()
 {
@@ -90,14 +160,14 @@ void main()
 	Tree tree;
 	for (int i = 0; i < n; i++)
 	{
-		tree.insert(rand() % 100, tree.getRoot());
+		tree.insert(rand() % 100);
 	}
-	tree.print(tree.getRoot());
+	tree.print();
 	cout << endl;
 
-	cout << "Минимальное значение в дереве: " << tree.minValue(tree.getRoot()) << endl;
-	cout << "Максимальное значение в дереве: " << tree.maxValue(tree.getRoot()) << endl;
-	cout << "Сумма элементов дерева: " << tree.Sum(tree.getRoot()) << endl;
-	cout << "Количество элементов дерева: " << tree.Count(tree.getRoot()) << endl;
+	cout << "Минимальное значение в дереве: " << tree.minValue() << endl;
+	cout << "Максимальное значение в дереве: " << tree.maxValue() << endl;
+	cout << "Сумма элементов дерева: " << tree.Sum() << endl;
+	cout << "Количество элементов дерева: " << tree.Count() << endl;
 	cout << "Среднее арифметическое элементов дерева: " << tree.Avg() << endl;
 }
